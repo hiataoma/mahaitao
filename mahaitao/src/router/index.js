4f54fba6df0b7d523be44f4fc67f99633e28e615
马海涛 <mahaitao@photonpay.com>
2021-08-04 16:24:27 +0800

    feat:新增备注

--- mahaitao/src/router/index.js
+++ mahaitao/src/router/index.js
@@ -11,8 +11,13 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/descriptions',
+    name: 'descriptions',
+    component: () => import(/* webpackChunkName: "about" */ '../components/descriptions')
+  },
+  {
+    path: '/componnets',
+    name: 'componnets',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
