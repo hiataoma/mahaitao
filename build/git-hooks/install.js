c0b2a9d3933efb58788c5c3f9f830535780bd073
马海涛 <mahaitao@photonpay.com>
2021-08-09 16:32:43 +0800

    fix: 修改

---
+++ build/git-hooks/install.js
@@ -0,0 +1,11 @@
+'use strict'
+const chalk = require('chalk')
+
+function exec(cmd) {
+  return require('child_process').execSync(cmd).toString().trim()
+}
+
+exec('git config core.hooksPath build/git-hooks')
+exec('chmod +x build/git-hooks/commit-msg')
+
+console.log(chalk.green('Successfully installed git hooks!'))
